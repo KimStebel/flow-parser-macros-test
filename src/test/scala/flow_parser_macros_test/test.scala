245c89d5ed042b8e509ccaee3b57e98c0c824355
Kim Stebel <kim.stebel@ovoenergy.com>
2017-01-16 16:54:51 +0000

    update tests to use real field names

--- src/test/scala/flow_parser_macros_test/test.scala
+++ src/test/scala/flow_parser_macros_test/test.scala
@@ -14,11 +14,6 @@ class D188Spec extends FunSpec with Matchers {
     val g387a = D188.Group387("f1a", "f2a", "f3a")
     val g387b = D188.Group387("f1b", "f2b", "f3b")
 
-    it("should have a group 387 with 3 fields") {
-      doNothing(g387a.field1, g387a.field2, g387a.field3)
-      //yup, this compiles
-    }
-
     it("should have 15 columns and a sequence of g387s") {
       D188.flowType should equal("D188")
       D188.Group386("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", Seq(g387a, g387b))
@@ -29,11 +24,20 @@ class D188Spec extends FunSpec with Matchers {
   
   describe("a group387 parser") {
     it("should parse a group 387 line into a case class") {
-      val testData = "387|f1|f2|f3|"
+      val testData = "387|123|abc|123|"
       val Parsed.Success(value, index) = D188.Group387.parser.parse(testData)
-      value should equal(D188.Group387("f1", "f2", "f3"))
+      value should equal(D188.Group387("123", "abc", "123"))
+      value.meterRegisterId should equal("123")
+      value.registerReading should equal("abc")
+      value.prepaymentUnitRate should equal("123")
+
       index should equal(testData.size)
     }
+  }
+  
+  describe("a group386 parser") {
+    it("should parse a group386 line and its subgroups") (pending)
+    
   }
   
 }
